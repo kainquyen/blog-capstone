cad0d5795362aae3c427dcbc26dd055339466b8d
Nguyễn Phú Khải <nkhai648@gmail.com>
2026-09-09 14:25:19 +0700

    abc

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: { port: 3000 },
@@ -11,7 +10,6 @@ export default defineConfig({
     viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
     tanstackStart(),
     viteReact(),
-    nitro(),
     tailwindcss(),
   ],
 })
